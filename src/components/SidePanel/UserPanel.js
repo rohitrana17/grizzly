e99db2ed631226005580d9f7ff52d99c2ee47b63
Rohit Rana <rohitrana1712001@gmail.com>
2021-06-21 15:46:57 +0530

    change app name

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -125,7 +125,7 @@ class UserPanel extends React.Component {
             {/* App Header */}
             <Header inverted floated="left" as="h2">
               <Icon name="code" />
-              <Header.Content>DevChat</Header.Content>
+              <Header.Content>Grizzly</Header.Content>
             </Header>
 
             {/* User Dropdown  */}
